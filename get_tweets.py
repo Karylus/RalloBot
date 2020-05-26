943a3ba714eafc30106bc1a9e85ce8174ad0de6c
Juan Pedro Moreno <57725792+Karylus@users.noreply.github.com>
2020-05-26 19:49:12 +0200

    Add files via upload

---
+++ get_tweets.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+
+#http://www.tweepy.org/
+import tweepy
+
+#Get your Twitter API credentials and enter them here
+consumer_key = "fkPjHCeVVZ24wyCru7lpEen5R"
+consumer_secret = "tfc9ySPXbD4LM3hSVkeH4axMGIokHiwjZSFqMa6RcqtNVqtsP3"
+access_key = "1023278501988847616-i6pnbRUcrttGH9eE9uuBVmrgzI68N3"
+access_secret = "8qwldPWgmsUv0oACOThdzLv0mL8L0pCXCX3VH8LB7jJOG"
+#method to get a user's last tweets
+def get_tweets(username):
+
+	#http://tweepy.readthedocs.org/en/v3.1.0/getting_started.html#api
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_key, access_secret)
+	api = tweepy.API(auth)
+
+	#set count to however many tweets you want
+	number_of_tweets = 150
+
+	#get tweets
+	tweets_for_csv = []
+	for tweet in tweepy.Cursor(api.user_timeline, screen_name = username, tweet_mode="extended").items(number_of_tweets):
+        #create array of tweet information: username, tweet id, date/time, text
+		tweets_for_csv.append([tweet.full_text.encode('utf-8').decode('utf-8')])
+
+	#write to a new csv file from the array of tweets
+	outfile = username + "_tweets.csv"
+	print ("writing to " + outfile)
+	with open(outfile, 'w+') as file:
+		writer = csv.writer(file, delimiter=',')
+		writer.writerows(tweets_for_csv)
+
+
+#if we're running this as a script
+if __name__ == '__main__':
+
+    #get tweets for username passed at command line
+    if len(sys.argv) == 2:
+        get_tweets(sys.argv[1])
+    else:
+        print ("Error: enter one username")
